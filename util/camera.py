0039d19249fdecba3aad71ccbc26595ef934def7
kaimin-mt <kaimin.liao@mthreads.com>
2024-05-21 16:17:12 +0800

    AABB raster
    1. make mesh for AABB
    2.rasterize large triangles

--- util/camera.py
+++ util/camera.py
@@ -3,15 +3,15 @@ import math
 from util import getWorld2View,getProjectionMatrix
 
 class Camera:
-    def __init__(self,position,rotation,fovX,fovY):
-        self.rotation=rotation
-        self.position=position
+    def __init__(self,viewtrans_position,viewtrans_rotation,fovX,fovY,z_near=0.01,z_far=100.0):
+        self.rotation_matrix=viewtrans_rotation.transpose()
+        self.position=-viewtrans_position
         self.fovX=fovX
         self.fovY=fovY
         
-        self.z_near=0.01
-        self.z_far=100.0
-        self.world2view_matrix=getWorld2View(self.rotation,self.position).transpose()
+        self.z_near=z_near
+        self.z_far=z_far
+        self.world2view_matrix=getWorld2View(viewtrans_rotation,viewtrans_position).transpose()
         self.project_matrix=getProjectionMatrix(znear=self.z_near, zfar=self.z_far, fovX=self.fovX, fovY=self.fovY).transpose()
         self.viewproj_matrix=np.matmul(self.world2view_matrix,self.project_matrix)
         self.viewproj_inv_matrix=np.linalg.inv(self.viewproj_matrix)
@@ -19,8 +19,8 @@ class Camera:
         return
     
 class View(Camera):
-    def __init__(self,position,rotation,fovX,fovY,image,image_name):
-        super(View,self).__init__(position,rotation,fovX,fovY)
+    def __init__(self,viewtrans_position,viewtrans_rotation,fovX,fovY,image,image_name):
+        super(View,self).__init__(viewtrans_position,viewtrans_rotation,fovX,fovY)
         self.image_name=image_name
         self.image=image
 
--- util/cg_torch.py
+++ util/cg_torch.py
@@ -61,7 +61,9 @@ def frustum_culling_aabb(frustumplane,aabb_origin,aabb_ext):
     '''
     Parameters:
         frustumplane - the planes of view frustum. [N,6,4]
+
         aabb_origin - the origin of Axis-Aligned Bounding Boxes. [M,3]
+
         aabb_ext - the extension of Axis-Aligned Bounding Boxes. [M,3]
     Returns:
         visibility - is visible. [N,M]
@@ -82,3 +84,152 @@ def frustum_culling_aabb(frustumplane,aabb_origin,aabb_ext):
     culling=(pushed_origin_dist<0).sum(-1)
     visibility=(culling==0)
     return visibility
+
+@torch.no_grad
+def make_aabb_mesh(aabb_origin:torch.Tensor,aabb_ext:torch.Tensor):
+    '''
+    Parameters:
+        aabb_origin - the origin of Axis-Aligned Bounding Boxes. [N,3]
+
+        aabb_ext - the extension of Axis-Aligned Bounding Boxes. [N,3]
+    Returns:
+        triangles - world coords of vertexs for each triangle. [N,6*2(triangle),3(vertex),3(position)]
+    '''
+    #   011-----111
+    #  /|       /|
+    # 010----110 |
+    # | 001----|101
+    # |/       |/
+    # 000-----100
+    assert(aabb_origin.shape[0]==aabb_ext.shape[0])
+
+    #vertex
+    V_000=aabb_origin-aabb_ext
+
+    V_001=V_000.clone()
+    V_001[:,2]+=2*aabb_ext[:,2]
+
+    V_010=V_000.clone()
+    V_010[:,1]+=2*aabb_ext[:,1]
+
+    V_100=V_000.clone()
+    V_100[:,0]+=2*aabb_ext[:,0]
+
+    V_011=V_001.clone()
+    V_011[:,1]+=2*aabb_ext[:,1]
+
+    V_101=V_001.clone()
+    V_101[:,0]+=2*aabb_ext[:,0]
+
+    V_110=V_010.clone()
+    V_110[:,0]+=2*aabb_ext[:,0]
+
+    V_111=aabb_origin+aabb_ext
+
+    #trangle
+    N=aabb_origin.shape[0]
+    triangles=torch.zeros((N,12,3,3),device=aabb_origin.device)
+
+    triangles[:,0,0]=V_000
+    triangles[:,0,1]=V_100
+    triangles[:,0,2]=V_010
+    triangles[:,1,0]=V_010
+    triangles[:,1,1]=V_100
+    triangles[:,1,2]=V_110
+
+    triangles[:,2,0]=V_001
+    triangles[:,2,1]=V_011
+    triangles[:,2,2]=V_101
+    triangles[:,3,0]=V_101
+    triangles[:,3,1]=V_011
+    triangles[:,3,2]=V_111
+
+    triangles[:,4,0]=V_010
+    triangles[:,4,1]=V_110
+    triangles[:,4,2]=V_011
+    triangles[:,5,0]=V_011
+    triangles[:,5,1]=V_110
+    triangles[:,5,2]=V_111
+
+    triangles[:,6,0]=V_000
+    triangles[:,6,1]=V_001
+    triangles[:,6,2]=V_100
+    triangles[:,7,0]=V_100
+    triangles[:,7,1]=V_001
+    triangles[:,7,2]=V_101
+
+    triangles[:,8,0]=V_000
+    triangles[:,8,1]=V_010
+    triangles[:,8,2]=V_001
+    triangles[:,9,0]=V_001
+    triangles[:,9,1]=V_010
+    triangles[:,9,2]=V_011
+
+    triangles[:,10,0]=V_100
+    triangles[:,10,1]=V_101
+    triangles[:,10,2]=V_110
+    triangles[:,11,0]=V_110
+    triangles[:,11,1]=V_101
+    triangles[:,11,2]=V_111
+
+    return triangles
+
+@torch.no_grad
+def raster_large_triangle(vertices_ndc_pos:torch.Tensor,vertices_property:torch.Tensor,H:int,W:int,device='cuda'):
+    '''
+    Parameters:
+        vertices_ndc_pos - NDC coords of vertex.  [N,3,3]
+
+        vertices_property - property of vertex to be rasterized. [M,N,3]
+
+        H,W - screen size
+    Returns:
+        (texture[H,W,M],depth[H,W,1])  
+    '''
+    if vertices_property is None:
+        N=vertices_ndc_pos.shape[0]
+        M=0
+    else:
+        (M,N,_)=vertices_property.shape
+    assert(N*H*W!=0)
+    assert(N*H*W<1024*1024*1024)
+
+    texture=torch.zeros((H,W,M),device=vertices_ndc_pos.device)
+    pixel_index=torch.meshgrid(torch.arange(0,H,device=device),torch.arange(0,W,device=device))
+    pixel_screen_pos=torch.stack((((pixel_index[1]+0.5)/W-0.5)*2,((pixel_index[0]+0.5)/H-0.5)*2),dim=-1).unsqueeze(2)#[H,W,1,2]
+    pixel_screen_pos=pixel_screen_pos.repeat((1,1,N,1))#[H,W,N,2]
+    
+    #Barycentric Coordinates
+    AB=vertices_ndc_pos[:,1,:2]-vertices_ndc_pos[:,0,:2]#[N,2]
+    BC=vertices_ndc_pos[:,2,:2]-vertices_ndc_pos[:,1,:2]#[N,2]
+    AC=vertices_ndc_pos[:,2,:2]-vertices_ndc_pos[:,0,:2]#[N,2]
+    AP=pixel_screen_pos-vertices_ndc_pos[:,0,:2]#[H,W,N,2]
+
+    area_B=AP[...,0]*AC[...,1]-AP[...,1]*AC[...,0]#|AP x AC| [H,W,N]
+    area_C=AB[...,0]*AP[...,1]-AB[...,1]*AP[...,0]#|AB x AP| [H,W,N]
+    triangle_area=AB[:,0]*BC[:,1]-AB[:,1]*BC[:,0]+1e-7#|AB x BC| [N]
+
+    barycentric_beta=area_B/triangle_area
+    barycentric_gamma=area_C/triangle_area
+    barycentric_alpha=1-barycentric_beta-barycentric_gamma
+    barycentric_coord=torch.stack((barycentric_alpha,barycentric_beta,barycentric_gamma),dim=-1)#[H,W,N,3]
+
+    mask=((barycentric_coord>0).sum(dim=-1)==3)
+
+    depth=(barycentric_coord*vertices_ndc_pos[:,:,2]).sum(dim=-1)
+    depth=depth*mask+(~mask)*1#[H,W,N]
+    if vertices_property is not None:
+        texture=(barycentric_coord.unsqueeze(2)*vertices_property).sum(dim=-1)#[H,W,M,N]
+    else:
+        texture=None
+
+    #occlusion
+    visibility_buffer=torch.argmin(depth,dim=2,keepdim=True)
+    depth=torch.gather(depth,2,visibility_buffer)
+    if vertices_property is not None:
+        property_list=[]
+        for i in range(M):
+            property_list.append(torch.gather(texture[:,:,i,:],2,visibility_buffer))
+        texture=torch.concat(property_list,dim=-1)
+
+    return depth,texture
