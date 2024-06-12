d913f481dedbd83d7a8991cb289ceb85bf9c7ed7
kaimin-mt <kaimin.liao@mthreads.com>
2024-06-12 10:52:53 +0800

    test code

--- profiler_test.py
+++ profiler_test.py
@@ -1,24 +1,35 @@
 import torch
-from gaussian_splatting.model import GaussiansRaster
+from gaussian_splatting.model import GaussianSplattingModel
+from gaussian_splatting import wrapper
 import numpy as np
 from matplotlib import pyplot as plt 
-from util import tiles2img_torch
+from util import tiles2img_torch,cg_torch
+import time
 
-tile_size=16
-tiles_num=[82,53]
+_xyz=torch.Tensor(np.load("profiler_input_data/xyz.npy")).cuda().requires_grad_()
+_scaling=torch.Tensor(np.load("profiler_input_data/scaling.npy")).cuda().requires_grad_()
+_rotation=torch.Tensor(np.load("profiler_input_data/rotation.npy")).cuda().requires_grad_()
+_opacity=torch.Tensor(np.load("profiler_input_data/opacity.npy")).cuda().requires_grad_()
+_features_dc=torch.Tensor(np.load("profiler_input_data/features_dc.npy")).cuda().requires_grad_()
 
-sorted_pointId=torch.Tensor(np.load("profiler_input_data/sorted_pointId.npy")).cuda().int()
-tile_start_index=torch.Tensor(np.load("profiler_input_data/tile_start_index.npy") ).cuda().int()
-mean2d=torch.Tensor(np.load("profiler_input_data/mean2d.npy")).cuda().requires_grad_()
-cov2d_inv=torch.Tensor(np.load("profiler_input_data/cov2d_inv.npy")).cuda().requires_grad_()
-color=torch.Tensor(np.load("profiler_input_data/color.npy") ).cuda().requires_grad_()
-opacities=torch.Tensor(np.load("profiler_input_data/opacities.npy") ).cuda().requires_grad_()
+view_matrix=torch.Tensor(np.load("profiler_input_data/view_matrix.npy")).cuda()
+view_project_matrix=torch.Tensor(np.load("profiler_input_data/view_project_matrix.npy")).cuda()
 
-tiles=torch.arange(1,tiles_num[0]*tiles_num[1]+1).unsqueeze(0).cuda().int()
+start_time=time.time()
+for i in range(100):
+    with torch.no_grad():
+        ndc_pos=cg_torch.world_to_ndc(_xyz,view_project_matrix)
+        translated_pos=cg_torch.world_to_view(_xyz,view_matrix)
+        visible_points,visible_points_num=GaussianSplattingModel.culling_and_sort(None,ndc_pos,translated_pos)
 
-img,transmitance=GaussiansRaster.apply(sorted_pointId,tile_start_index,mean2d,cov2d_inv,color,opacities,tiles,tile_size,tiles_num[0],tiles_num[1],840, 1297)
-img.mean().backward()
-with torch.no_grad():
-    img=tiles2img_torch(img,82,53)[:,:,0:840,0:1297]
+        scales=_scaling[visible_points].exp()
+        rotators=torch.nn.functional.normalize(_rotation[visible_points],dim=-1)
+        visible_positions=_xyz[visible_points]
+        visible_opacities=_opacity[visible_points].sigmoid()
+        visible_sh0=_features_dc[visible_points]
+    
+torch.cuda.synchronize()
+end_time=time.time()
+print((end_time-start_time)*1000/100,"ms")
 
-n=1
+
