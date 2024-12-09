c2f21efbae5aebd7d97c00e0e973129f246cd795
kaimin.liao <kaimin.liao@mthreads.com>
2024-12-09 16:40:41 +0800

    update profiler

--- profiler_iter.py
+++ profiler_iter.py
@@ -4,45 +4,43 @@ import typing
 from loader.InfoLoader import CameraInfo,ImageInfo
 from gaussian_splatting.scene import GaussianScene
 from gaussian_splatting.model import GaussianSplattingModel
-import training.loss
-from training.loss.ssim import SSIM
-import training.loss.ssim
 from training.training import ViewManager
-from training.densitycontroller import DensityControllerOurs
 import torch
-from util import tiles2img_torch,statistic_helper
+from util import tiles2img_torch
 from matplotlib import pyplot as plt 
 from training.arguments import OptimizationParams
 import time
+import fused_ssim
 
-plt_index=[0,14]
+all_scene=["bicycle","bonsai","counter","flowers","garden","kitchen","room","stump","treehill"]
+scene_str=all_scene[8]
 
 #load training data
 cameras_info:typing.Dict[int,CameraInfo]=None
 images_info:typing.List[ImageInfo]=None
 scene:GaussianScene=None
-cameras_info,images_info,scene,_,NerfNormRadius=TrainingDataLoader.load('./dataset/garden','images',0,-1)
+cameras_info,images_info,scene,_,NerfNormRadius=TrainingDataLoader.load('./dataset/{0}/A100_colmap-default_gaussian-splatting-default'.format(scene_str),'images',0,-1)
 image_size=images_info[0].image.size
 
 args = OptimizationParams(ArgumentParser(description="Training script parameters"))
-
+scene.load_ply('./output/{0}/chkpnt12800.ply'.format(scene_str))
 
 gaussian_model=GaussianSplattingModel(scene,0)
 gaussian_model.update_tiles_coord(image_size,8)
-(model_params,op_state_dict, first_iter,actived_sh_degree) = torch.load('output/sh0_4M.pth')
-gaussian_model.load_params(model_params)
-gaussian_model.actived_sh_degree=actived_sh_degree
+#(model_params,op_state_dict, first_iter,actived_sh_degree) = torch.load('output/sh0_4M.pth')
+#gaussian_model.load_params(model_params)
+#gaussian_model.actived_sh_degree=actived_sh_degree
 gaussian_model.rebuild_BVH(gaussian_model.chunk_size)
-l = [
-            {'params': [gaussian_model._xyz], 'lr': args.position_lr_init * 0.1, "name": "xyz"},
-            {'params': [gaussian_model._features_dc], 'lr': args.feature_lr, "name": "f_dc"},
-            {'params': [gaussian_model._features_rest], 'lr': args.feature_lr / 20.0, "name": "f_rest"},
-            {'params': [gaussian_model._opacity], 'lr': args.opacity_lr, "name": "opacity"},
-            {'params': [gaussian_model._scaling], 'lr': args.scaling_lr, "name": "scaling"},
-            {'params': [gaussian_model._rotation], 'lr': args.rotation_lr, "name": "rotation"}
-        ]
-optimizer = torch.optim.Adam(l, lr=0.0, eps=1e-15)
-optimizer.load_state_dict(op_state_dict)
+# l = [
+#             {'params': [gaussian_model._xyz], 'lr': args.position_lr_init * 0.1, "name": "xyz"},
+#             {'params': [gaussian_model._features_dc], 'lr': args.feature_lr, "name": "f_dc"},
+#             {'params': [gaussian_model._features_rest], 'lr': args.feature_lr / 20.0, "name": "f_rest"},
+#             {'params': [gaussian_model._opacity], 'lr': args.opacity_lr, "name": "opacity"},
+#             {'params': [gaussian_model._scaling], 'lr': args.scaling_lr, "name": "scaling"},
+#             {'params': [gaussian_model._rotation], 'lr': args.rotation_lr, "name": "rotation"}
+#         ]
+#optimizer = torch.optim.Adam(l, lr=0.0, eps=1e-15)
+#optimizer.load_state_dict(op_state_dict)
 
 
 trainingset=[c for idx, c in enumerate(images_info) if idx % 8 != 0]
@@ -56,12 +54,10 @@ camera_center=torch.Tensor(view_manager.camera_center_tensor).cuda()
 camera_focal=torch.Tensor(view_manager.camera_focal_tensor).cuda()
 ground_truth=torch.Tensor(view_manager.view_gt_tensor).cuda()
 total_views_num=view_matrix.shape[0]
-ssim_module=training.loss.ssim.SSIM(data_range=1.0).cuda()
-statistic_helper.StatisticsHelperInst.reset(gaussian_model._xyz.shape[-2],gaussian_model._xyz.shape[-1])
-statistic_helper.StatisticsHelperInst.start()
-density_controller=DensityControllerOurs(args)
+
+
 start=time.monotonic()
-for i in range(0,view_matrix.shape[0],1):
+for i in range(0,10):
     with torch.no_grad():
         view_matrix_batch=view_matrix[i:i+1]
         view_project_matrix_batch=view_project_matrix[i:i+1]
@@ -69,19 +65,11 @@ for i in range(0,view_matrix.shape[0],1):
         camera_center_batch=camera_center[i:i+1]
         ground_truth_batch=ground_truth[i:i+1].contiguous()
 
-    tile_img,tile_transmitance=gaussian_model.render(view_matrix_batch,view_project_matrix_batch,camera_focal_batch,camera_center_batch,
-                                                     None,None)
+    tile_img,tile_transmitance=gaussian_model.render(view_matrix_batch,view_project_matrix_batch,camera_focal_batch,camera_center_batch,None,None)
     img=tiles2img_torch(tile_img,gaussian_model.cached_tiles_size[0],gaussian_model.cached_tiles_size[1])[...,:image_size[1],:image_size[0]].contiguous()
-    l1_loss=training.loss.l1_loss(img,ground_truth_batch)
-    ssim_loss=ssim_module(img,ground_truth_batch).mean()
-    loss=(1.0-0.2)*l1_loss+0.2*(1-ssim_loss)
-    loss.backward()
-    statistic_helper.StatisticsHelperInst.backward_callback()
-    optimizer.zero_grad(set_to_none = True)
+    img.mean().backward()
+    #optimizer.zero_grad(set_to_none = True)
 
-    # with torch.no_grad():
-    #     if i in plt_index:
-    #         diff_img=torch.abs(img - ground_truth_batch)
 torch.cuda.synchronize()
 end=time.monotonic()
 print("takes:{0} ms".format(end-start))#19ms
--- profiler_raster.py
+++ profiler_raster.py
@@ -8,58 +8,54 @@ import time
 
 imgH=1036
 imgW=1600
-tilesize=8
+tilesize=16
 tilesnumX=int(np.ceil(imgW/tilesize))
 tilesnumY=int(np.ceil(imgH/tilesize))
 
 if tilesize==8:
-    color=torch.Tensor(np.load("profiler_input_data/profiler_input_data_8x8/color.npy")).cuda().requires_grad_()
-    cov2d_inv=torch.Tensor(np.load("profiler_input_data/profiler_input_data_8x8/cov2d_inv.npy")).cuda().requires_grad_()
-    mean2d=torch.Tensor(np.load("profiler_input_data/profiler_input_data_8x8/mean2d.npy")).cuda().requires_grad_()
-    opacities=torch.Tensor(np.load("profiler_input_data/profiler_input_data_8x8/opacities.npy")).cuda().requires_grad_()
+    color=torch.Tensor(np.load("profiler_input_data/8x8/color.npy")).cuda().requires_grad_()
+    inv_cov2d=torch.Tensor(np.load("profiler_input_data/8x8/inv_cov2d.npy")).cuda().requires_grad_()
+    ndc_pos=torch.Tensor(np.load("profiler_input_data/8x8/ndc_pos.npy")).cuda().requires_grad_()
+    opacities=torch.Tensor(np.load("profiler_input_data/8x8/opacities.npy")).cuda().requires_grad_()
+    sorted_pointId=torch.tensor(np.load("profiler_input_data/8x8/sorted_pointId.npy"),dtype=torch.int32,device='cuda')
+    tile_start_index=torch.tensor(np.load("profiler_input_data/8x8/tile_start_index.npy"),dtype=torch.int32,device='cuda')
+    tiles=torch.tensor(np.load("profiler_input_data/8x8/tiles.npy"),dtype=torch.int32,device='cuda')
 
-    sorted_pointId=torch.tensor(np.load("profiler_input_data/profiler_input_data_8x8/sorted_pointId.npy"),dtype=torch.int32,device='cuda')
-    tile_start_index=torch.tensor(np.load("profiler_input_data/profiler_input_data_8x8/tile_start_index.npy"),dtype=torch.int32,device='cuda')
-    tiles=torch.tensor(np.load("profiler_input_data/profiler_input_data_8x8/tiles.npy"),dtype=torch.int32,device='cuda')
+    grad_color=torch.Tensor(np.load("profiler_input_data/8x8/grad_color.npy")).cuda()
+    grad_inv_cov2d=torch.Tensor(np.load("profiler_input_data/8x8/grad_inv_cov2d.npy")).cuda()
+    grad_ndc_pos=torch.Tensor(np.load("profiler_input_data/8x8/grad_ndc_pos.npy")).cuda()
+    grad_opacities=torch.Tensor(np.load("profiler_input_data/8x8/grad_opacities.npy")).cuda()
+
+
 elif tilesize==16:
-    color=torch.Tensor(np.load("profiler_input_data/color.npy")).cuda().requires_grad_()
-    cov2d_inv=torch.Tensor(np.load("profiler_input_data/cov2d_inv.npy")).cuda().requires_grad_()
-    mean2d=torch.Tensor(np.load("profiler_input_data/mean2d.npy")).cuda().requires_grad_()
-    opacities=torch.Tensor(np.load("profiler_input_data/opacities.npy")).cuda().requires_grad_()
+    color=torch.Tensor(np.load("profiler_input_data/16x16/color.npy")).cuda().requires_grad_()
+    inv_cov2d=torch.Tensor(np.load("profiler_input_data/16x16/inv_cov2d.npy")).cuda().requires_grad_()
+    ndc_pos=torch.Tensor(np.load("profiler_input_data/16x16/ndc_pos.npy")).cuda().requires_grad_()
+    opacities=torch.Tensor(np.load("profiler_input_data/16x16/opacities.npy")).cuda().requires_grad_()
+    sorted_pointId=torch.tensor(np.load("profiler_input_data/16x16/sorted_pointId.npy"),dtype=torch.int32,device='cuda')
+    tile_start_index=torch.tensor(np.load("profiler_input_data/16x16/tile_start_index.npy"),dtype=torch.int32,device='cuda')
+    tiles=torch.tensor(np.load("profiler_input_data/16x16/tiles.npy"),dtype=torch.int32,device='cuda')
 
-    sorted_pointId=torch.tensor(np.load("profiler_input_data/sorted_pointId.npy"),dtype=torch.int32,device='cuda')
-    tile_start_index=torch.tensor(np.load("profiler_input_data/tile_start_index.npy"),dtype=torch.int32,device='cuda')
-    tiles=torch.tensor(np.load("profiler_input_data/tiles.npy"),dtype=torch.int32,device='cuda')
-
-
-#check result
-img_ord=torch.Tensor(np.load("profiler_input_data/img.npy")).cuda()
-transmitance_ord=torch.Tensor(np.load("profiler_input_data/transmitance.npy")).cuda()
-color_grad=torch.Tensor(np.load("profiler_input_data/color_grad.npy")).cuda()
-cov2d_inv_grad=torch.Tensor(np.load("profiler_input_data/cov2d_inv_grad.npy")).cuda()
-mean2d_grad=torch.Tensor(np.load("profiler_input_data/mean2d_grad.npy")).cuda()
-opacities_grad=torch.Tensor(np.load("profiler_input_data/opacities_grad.npy")).cuda()
+    grad_color=torch.Tensor(np.load("profiler_input_data/16x16/grad_color.npy")).cuda()
+    grad_inv_cov2d=torch.Tensor(np.load("profiler_input_data/16x16/grad_inv_cov2d.npy")).cuda()
+    grad_ndc_pos=torch.Tensor(np.load("profiler_input_data/16x16/grad_ndc_pos.npy")).cuda()
+    grad_opacities=torch.Tensor(np.load("profiler_input_data/16x16/grad_opacities.npy")).cuda()
 
 
 
 
-img,trans=wrapper.rasterize_2d_gaussian(sorted_pointId,tile_start_index,mean2d,cov2d_inv,color,opacities,tiles,tilesize,tilesnumX,tilesnumY,imgH,imgW)
+img,trans=wrapper.rasterize_2d_gaussian(sorted_pointId,tile_start_index,ndc_pos,inv_cov2d,color,opacities,tiles,tilesize,tilesnumX,tilesnumY,imgH,imgW)
 img.mean().backward()
-with torch.no_grad():
-    img=tiles2img_torch(img,tilesnumX,tilesnumY)[...,:imgH,:imgW]
-    trans=tiles2img_torch(trans.unsqueeze(2),tilesnumX,tilesnumY)[...,:imgH,:imgW]
 
-print("diff img:            ",(img-img_ord).abs().sum())
-print("diff transmitance:   ",(trans-transmitance_ord).abs().sum())
-print("diff color_grad:     ",(color.grad-color_grad).abs().max())
-print("diff cov2d_inv_grad: ",(cov2d_inv.grad-cov2d_inv_grad).abs().max())
-print("diff mean2d_grad:    ",(mean2d.grad-mean2d_grad).abs().max())
-print("diff opacities_grad: ",(opacities.grad-opacities_grad).abs().max())
+print("diff color_grad:     ",(color.grad-grad_color).abs().max())
+print("diff inv_cov2d_grad: ",(inv_cov2d.grad-grad_inv_cov2d).abs().max())
+print("diff mean2d_grad:    ",(ndc_pos.grad-grad_ndc_pos).abs().max())
+print("diff opacities_grad: ",(opacities.grad-grad_opacities).abs().max())
 
 
 start_time=time.time()
 for i in range(100):
-    img,trans=wrapper.rasterize_2d_gaussian(sorted_pointId,tile_start_index,mean2d,cov2d_inv,color,opacities,tiles,
+    img,trans=wrapper.rasterize_2d_gaussian(sorted_pointId,tile_start_index,ndc_pos,inv_cov2d,color,opacities,tiles,
                                   tilesize,tilesnumX,tilesnumY,imgH,imgW)
     img.mean().backward()
 torch.cuda.synchronize()
