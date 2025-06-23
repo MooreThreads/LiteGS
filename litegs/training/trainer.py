be2b80d113282be976c655070a63560a03223286
kaimin.liao <kaimin.liao@mthreads.com>
2025-06-23 15:12:23 +0800

    Update trainer.py

--- litegs/training/trainer.py
+++ litegs/training/trainer.py
@@ -106,7 +106,6 @@ def start(lp:arguments.ModelParams,op:arguments.OptimizationParams,pp:arguments.
                 l1_loss=__l1_loss(img,gt_image)
                 ssim_loss:torch.Tensor=1-fused_ssim.fused_ssim(img,gt_image)
                 loss=(1.0-op.lambda_dssim)*l1_loss+op.lambda_dssim*ssim_loss
-                loss+=culled_opacity.sigmoid().mean()*1e-6
                 loss.backward()
                 if StatisticsHelperInst.bStart:
                     StatisticsHelperInst.backward_callback()
