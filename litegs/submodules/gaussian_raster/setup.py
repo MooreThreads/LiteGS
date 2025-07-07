ddb7b925909e739af20a25d8bc61256da9e282ac
kaimin.liao <kaimin.liao@mthreads.com>
2025-07-07 12:51:02 +0800

    bug fix: pip install

--- litegs/submodules/gaussian_raster/setup.py
+++ litegs/submodules/gaussian_raster/setup.py
@@ -1,22 +1,37 @@
 from setuptools import setup
-from torch.utils.cpp_extension import CUDAExtension, BuildExtension
+from torch.utils.cpp_extension import CUDAExtension, BuildExtension,COMMON_NVCC_FLAGS
 
-setup(
-    name="litegs_fused",
-    packages=['litegs_fused'],
-    package_dir={'litegs_fused':"."},
-    ext_modules=[
-        CUDAExtension(
-            name="litegs_fused",
-            sources=[
-            "binning.cu",
-            "compact.cu",
-            "cuda_errchk.cpp",
-            "ext_cuda.cpp",
-            "raster.cu",
-            "transform.cu"])
-        ],
-    cmdclass={
-        'build_ext': BuildExtension
-    }
-)
+def remove_unwanted_pytorch_nvcc_flags():
+    REMOVE_NVCC_FLAGS = [
+        '-D__CUDA_NO_HALF_OPERATORS__',
+        '-D__CUDA_NO_HALF_CONVERSIONS__',
+        '-D__CUDA_NO_BFLOAT16_CONVERSIONS__',
+        '-D__CUDA_NO_HALF2_OPERATORS__',
+    ]
+    for flag in REMOVE_NVCC_FLAGS:
+        try:
+            COMMON_NVCC_FLAGS.remove(flag)
+        except ValueError:
+            pass
+
+if __name__ == '__main__':
+    remove_unwanted_pytorch_nvcc_flags()
+    setup(
+        name="litegs_fused",
+        packages=['litegs_fused'],
+        package_dir={'litegs_fused':"."},
+        ext_modules=[
+            CUDAExtension(
+                name="litegs_fused",
+                sources=[
+                "binning.cu",
+                "compact.cu",
+                "cuda_errchk.cpp",
+                "ext_cuda.cpp",
+                "raster.cu",
+                "transform.cu"])
+            ],
+        cmdclass={
+            'build_ext': BuildExtension
+        }
+    )
