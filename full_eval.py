20e644c6b4251e118ede36ac0b0600626829622d
kaimin.liao <kaimin.liao@mthreads.com>
2025-07-20 15:48:23 +0800

    Update full_eval.py

--- full_eval.py
+++ full_eval.py
@@ -128,7 +128,7 @@ if not args.skip_training:
             for target_primitives in target_primitives_list[scene_name]:
                 scene_output_path=os.path.join(args.output_path,scene_name+'-{}k'.format(int(target_primitives/1000)))
                 print("scene:{} #primitive:{}".format(scene_name,target_primitives))
-                os.system("python example_train.py -s {0} -m {1} --eval --sh_degree 3 --target_primitives {2} {3} {4} >> train.txt".format(
+                os.system("python example_train.py -s {0} -m {1} --eval --sh_degree 3 --target_primitives {2} {3} {4} >> takes_time.txt".format(
                     scene_input_path,
                     scene_output_path,
                     target_primitives,
@@ -136,10 +136,10 @@ if not args.skip_training:
                     reg_config[dataset]
                 ))
             #full
-            scene_output_path=os.path.join(args.output_path,scene_name+'-big')
             target_primitives=big_budgets[scene_name]
+            scene_output_path=os.path.join(args.output_path,scene_name+'-{}k'.format(int(target_primitives/1000)))
             print("scene:{} #primitive:{}".format(scene_name,target_primitives))
-            os.system("python example_train.py -s {0} -m {1} --eval --sh_degree 3 --target_primitives {2} {3} {4} >> train.txt".format(
+            os.system("python example_train.py -s {0} -m {1} --eval --sh_degree 3 --target_primitives {2} {3} {4} >> takes_time.txt".format(
                     scene_input_path,
                     scene_output_path,
                     target_primitives,
@@ -153,5 +153,6 @@ for dataset,scenes in datasets.items():
         for target_primitives in target_primitives_list[scene_name]:
             scene_output_path=os.path.join(args.output_path,scene_name+'-{}k'.format(int(target_primitives/1000)))
             os.system("python example_metrics.py -s {0} -m {1} --sh_degree 3 {2}  >> output.txt".format(scene_input_path,scene_output_path,img_config[dataset]))
-        scene_output_path=os.path.join(args.output_path,scene_name+'-big')
+        target_primitives=big_budgets[scene_name]
+        scene_output_path=os.path.join(args.output_path,scene_name+'-{}k'.format(int(target_primitives/1000)))
         os.system("python example_metrics.py -s {0} -m {1} --sh_degree 3 {2}  >> output.txt".format(scene_input_path,scene_output_path,img_config[dataset]))
