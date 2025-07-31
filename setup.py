9ed6c12ea847f8166c2fe30cc9536645a2d5bd95
kaimin.liao <kaimin.liao@mthreads.com>
2025-07-31 11:34:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+from torch.utils.cpp_extension import CUDAExtension, BuildExtension,COMMON_NVCC_FLAGS
+
 def get_cuda_modules():
     from torch.utils.cpp_extension import CUDAExtension
     ext_modules = [
@@ -30,24 +32,39 @@ def get_cmdclass():
     from torch.utils.cpp_extension import BuildExtension
     return {'build_ext': BuildExtension}
 
-setup(
-    name="lite-gaussian-splatting",
-    version="1.0.1",
-    author="Kaimin Liao",
-    author_email="kaiminliao@gmail.com",
-    description="A High-Performance Modular Framework for Gaussian Splatting Training",
-    long_description_content_type="text/markdown",
-    url="https://github.com/MooreThreads/LiteGS",
-    packages=find_packages(where=".", include=["litegs", "litegs.*"])+["fused_ssim"],
-    package_dir={"litegs": "litegs","fused_ssim":"litegs/submodules/fused_ssim/fused_ssim"},
-    setup_requires=["torch","wheel"],
-    install_requires=["torch","wheel","numpy","plyfile","tqdm","pillow"],
-    ext_modules=get_cuda_modules(),
-    include_package_data=True,
-    cmdclass=get_cmdclass(),
-    python_requires=">=3.8",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-)
+def remove_unwanted_pytorch_nvcc_flags():
+    REMOVE_NVCC_FLAGS = [
+        '-D__CUDA_NO_HALF_OPERATORS__',
+        '-D__CUDA_NO_HALF_CONVERSIONS__',
+        '-D__CUDA_NO_BFLOAT16_CONVERSIONS__',
+        '-D__CUDA_NO_HALF2_OPERATORS__',
+    ]
+    for flag in REMOVE_NVCC_FLAGS:
+        try:
+            COMMON_NVCC_FLAGS.remove(flag)
+        except ValueError:
+            pass
+
+if __name__ == '__main__':
+    remove_unwanted_pytorch_nvcc_flags()
+    setup(
+        name="lite-gaussian-splatting",
+        version="1.0.1",
+        author="Kaimin Liao",
+        author_email="kaiminliao@gmail.com",
+        description="A High-Performance Modular Framework for Gaussian Splatting Training",
+        long_description_content_type="text/markdown",
+        url="https://github.com/MooreThreads/LiteGS",
+        packages=find_packages(where=".", include=["litegs", "litegs.*"])+["fused_ssim"],
+        package_dir={"litegs": "litegs","fused_ssim":"litegs/submodules/fused_ssim/fused_ssim"},
+        setup_requires=["torch","wheel"],
+        install_requires=["torch","wheel","numpy","plyfile","tqdm","pillow"],
+        ext_modules=get_cuda_modules(),
+        include_package_data=True,
+        cmdclass=get_cmdclass(),
+        python_requires=">=3.8",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+        ],
+    )
