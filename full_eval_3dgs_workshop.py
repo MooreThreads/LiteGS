c5a6defa4d15c497616bc392f72af115d2f64777
kaimin.liao <kaimin.liao@mthreads.com>
2025-10-24 17:49:16 +0800

    update scripts

--- full_eval_3dgs_workshop.py
+++ full_eval_3dgs_workshop.py
@@ -11,6 +11,8 @@
 
 import os
 from argparse import ArgumentParser
+import subprocess
+import numpy as np
 
 
 parser = ArgumentParser(description="Full evaluation script parameters")
@@ -19,14 +21,26 @@ parser.add_argument('--source_path', required=True, type=str)
 args, _ = parser.parse_known_args()
 
 
-training_config="--sh_degree 3 --source_type slam -s {0} -m {1} --target_primitives 1000000 --iterations 5000 --position_lr_max_steps 5000 --position_lr_final 0.000016 --densification_interval 2 --learnable_viewproj"
+training_config="--sh_degree 3 --source_type slam -s {0} -m {1} --target_primitives 1000000 --iterations 4500 --position_lr_max_steps 4500 --position_lr_final 0.000016 --densification_interval 2 --learnable_viewproj"
 
 metrics_config="--sh_degree 3 --source_type slam -s {0} -m {1} --learnable_viewproj"
 
 scenes = os.listdir(args.source_path)
 
-for scene in scenes:
-    os.system("python example_train.py "+training_config.format(os.path.join(args.source_path,scene),os.path.join(args.output_path,scene)))
+# for scene in scenes:
+#     os.system("python example_train.py "+training_config.format(os.path.join(args.source_path,scene),os.path.join(args.output_path,scene)))
 
-for scene in scenes:
-    os.system("python example_metrics.py "+metrics_config.format(os.path.join(args.source_path,scene),os.path.join(args.output_path,scene)))
+results=[]
+for scene in scenes[:2]:
+    process = subprocess.Popen("python example_metrics.py "+metrics_config.format(os.path.join(args.source_path,scene),os.path.join(args.output_path,scene)), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    stdout, stderr = process.communicate()
+    print(stderr)
+    print(stdout)
+
+    index=stdout.find('  PSNR : ')
+    if index!=-1:
+        end=stdout[index+9:].find('\n')
+        psnr=float(stdout[index+9:index+9+end])
+        results.append(psnr)
+print('Finish')
+print('PSNR avg:',np.array(results).mean())
